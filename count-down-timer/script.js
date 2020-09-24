57ab3d7ea890dca632ad02d69978a9f5b0f0ef3b
vilktor370 <66043167+vilktor370@users.noreply.github.com>
2020-09-24 10:40:30 -0400

    Delete script.js

--- count-down-timer/script.js
+++
@@ -1,8 +0,0 @@
-let time=600;
-setInterval (myFunction,1000)
-const countdown=document.getElementById("mins");
-function myFunction() {
-    const mins=Math.floor(time/60);
-    countdown.innerHTML()=mins;
-    time--;
-}
